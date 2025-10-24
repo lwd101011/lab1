44918b0329a12efed40b04d2ada5c68cb543fbb0
lwd101011 <466712445@qq.com>
2025-10-24 18:18:23 +0800

    双操作数操作符处理

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,6 +19,11 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
     connect(ui->btnNum9,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
 
+    connect(ui->btnPlus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnMinus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnMultiple,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnDivide,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+
 }
 
 MainWindow::~MainWindow()
@@ -25,29 +31,111 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+QString MainWindow::calculation(bool *ok = NULL)
+{
+    double result = 0;
+    if(operands.size() == 2 && opcodes.size() > 0 )
+    {
+        // 取操作数
+        double operand1 = operands.front().toDouble();
+        operands.pop_front();
+        double operand2 = operands.front().toDouble();
+        operands.pop_front();
+
+        //取操作符
+        QString op = opcodes.front();
+        opcodes.pop_front();
+
+        if(op == "+"){
+            result = operand1 + operand2;
+        }
+        else if(op == "-"){
+            result = operand1 - operand2;
+        }
+        if(op == "*"){
+            result = operand1 * operand2;
+        }
+        if(op == "/"){
+            result = operand1 / operand2;
+        }
+
+        ui->statusbar->showMessage(QString("calculation is in progress: operand is %1, opcode is %2").arg(operands.size()).arg(opcodes.size()));
+    }
+    else
+        ui->statusbar->showMessage(QString("operand is %1, opcode is %2").arg(operands.size()).arg(opcodes.size()));
+
+    return QString::number(result);
+}
+
 void MainWindow::btnNumClicked()
 {
-    QString str = ui->display->text();
-    str += qobject_cast<QPushButton*>(sender())->text();
+    QString digit = qobject_cast<QPushButton*>(sender())->text();
 
-    ui->display->setText(str);
-   // ui->statusbar->showMessage(qobject_cast<QPushButton*>(sender())->text() + "btn clicked");
+    if(digit == "0" && operand == "0")
+        digit = "";
+
+    if(operand == "0" && digit != "0")
+        operand = "";
+
+    operand += digit;
+
+     ui->display->setText(operand);
 
 }
 
+
 void MainWindow::on_btnPeriod_clicked()
 {
-    QString str = ui->display->text();
-    if(!str.contains("."))
-        str += qobject_cast<QPushButton*>(sender())->text();
-    ui->display->setText(str);
+    if(!operand.contains("."))
+        operand += qobject_cast<QPushButton*>(sender())->text();
+    ui->display->setText(operand);
 }
 
 
 void MainWindow::on_btnDel_clicked()
 {
-    QString str = ui->display->text();
-    str = str.left(str.length()-1);
-    ui->display->setText(str);
+    operand = operand.left(operand.length()-1);
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnClear_clicked()
+{
+    operand.clear();
+    ui->display->setText(operand);
+}
+
+void MainWindow::btnBinaryOperatorClicked()
+{
+    ui->statusbar->showMessage("last operand" + operand);
+    QString opcode = qobject_cast<QPushButton*>(sender())->text();
+    qDebug()<<opcode;
+
+    if(operand != "")
+    {
+        operands.push_back(operand);
+        operand = "";
+
+        opcodes.push_back(opcode);
+
+
+        QString result =  calculation();
+
+        ui->display->setText(result);
+    }
+
+}
+
+void MainWindow::on_btnEqual_clicked()
+{
+    if(operand != "")
+    {
+        operands.push_back(operand);
+        operand = "";
+    }
+
+    QString result =  calculation();
+
+    ui->display->setText(result);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QLabel>
+#include <QStack>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,12 +19,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    QString operand;
+    QString opcode;
+    QStack<QString> operands;
+    QStack<QString> opcodes;
+
+    QString calculation(bool *ok);
+
 private slots:
     void btnNumClicked();
+    void btnBinaryOperatorClicked();
 
     void on_btnPeriod_clicked();
 
     void on_btnDel_clicked();
+
+    void on_btnClear_clicked();
+
+    void on_btnEqual_clicked();
 
 private:
     Ui::MainWindow *ui;
